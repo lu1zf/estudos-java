2d14cdffcbed0e8c854aa11d79b99a47af266196
Luiz Silva <luizsilva.ee@gmail.com>
2021-01-26 22:07:37 -0300

    [fix] tabualção

---
+++ MeuPrograma.java
@@ -0,0 +1,22 @@
+class MeuPrograma {
+	public static void main(String[] args) {
+		
+		int idade = 23;
+		System.out.println(idade);
+		
+		double pi = 3.14;
+		System.out.println(pi);
+		
+		boolean verdade = true;
+		System.out.println(verdade);
+		
+		boolean menorDeIdade = idade < 18;
+		System.out.println(menorDeIdade);
+		
+		/* variáveis do tipo char sempre devem ter apenas 1 caractere entre
+			 entre aspas simples. Sempre.
+		*/
+		char letra = 'a';
+		System.out.println(letra);
+	}
+}
