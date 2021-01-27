e4405bfb9cf04fd695f164f8cf4299bf1ba688f2
Luiz Silva <luizsilva.ee@gmail.com>
2021-01-26 22:43:49 -0300

    Exemplo de casting de variavéis

---
+++ 3. variaveis e tipos primitivos/ExemploCasting.java
@@ -0,0 +1,17 @@
+class ExemploCasting {
+  
+  public static void main(String[] args){
+    
+    double d3 = 3.14;
+    int i = (int) d3; // foi feito o casting para que o valor de d3 se tornasse inteiro
+    
+    System.out.println(i);
+    
+    long x = 10000;
+    int j = (int) x;
+    
+    System.out.println(j);
+    
+  }  
+  
+}
